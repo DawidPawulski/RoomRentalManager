0003ea2106f3d64752b08bf66caaf109e80c9bc4
JCKolodziej <49226039+JCKolodziej@users.noreply.github.com>
2020-01-15 21:16:43 +0100

    Add Vue components and vue pages

--- RoomRentalManager.FrontEnd/src/App.vue
+++ RoomRentalManager.FrontEnd/src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="app">
+        <Navbar/>
         <Home msg="Hello world!" />
     </div>
 </template>
 
 <script>
     import Home from './components/Home.vue';
+    import Navbar from './components/Navbar.vue';
 
     export default {
         name: 'app',
         components: {
-            Home
+            Home,
+            Navbar
         }
     };
 </script>
