34ada449ee0e14633ceab6aa8e9b71e444d2182a
JCKolodziej <49226039+JCKolodziej@users.noreply.github.com>
2020-01-23 09:37:32 +0100

    Add simple market table, with a way to pass down data to child component in vue

--- RoomRentalManager.FrontEnd/src/components/MarketTable.vue
+++ RoomRentalManager.FrontEnd/src/components/MarketTable.vue
@@ -1,7 +1,29 @@
 <template>
+    <table>
+        <thead>
+            <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="advert in AdvertList" v-bind:key="advert.date">
+                <td>{{ advert.image }}</td>
+                <td>{{ advert.title }}</td>
+                <td>{{ advert.date }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
+    export default {
+        name: 'MarketTable',
+        props: {
+            AdvertList: Array
+        }
+    };
 </script>
 
 <style scoped>
--- RoomRentalManager.FrontEnd/src/views/MainPage.vue
+++ RoomRentalManager.FrontEnd/src/views/MainPage.vue
@@ -1,12 +1,36 @@
 <template>
-    <div>main</div>
+    <div><market :AdvertList="Adverts"></market></div>
 </template>
 
 <script>
+    import MarketTable from '../components/MarketTable';
+
     export default {
         name: 'MainPage',
-        props: {
-
+        data (){
+            return {
+                Adverts: [
+                {
+                    image: "img1",
+                    title: "title1",
+                    date: "1"
+                },
+                {
+                    image: "img2",
+                    title: "title2",
+                    date: "2"
+                },
+                {
+                    image: "img3",
+                    title: "title3",
+                    date: "3"
+                }
+            ]
+            }
+            
+        },
+        components: {
+            'market': MarketTable
         }
     }
 </script>
