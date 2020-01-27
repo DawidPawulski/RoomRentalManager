ed2b7eee0316c842aa29f7e2a0f600c8308c6b59
Dawid Pawulski <dpawulski@gmail.com>
2020-01-27 10:04:59 +0100

    Create user controler

--- RoomRentalManager/Startup.cs
+++ RoomRentalManager/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,8 @@ namespace RoomRentalManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<Models.RRM_DBContext>(options =>
+        options.UseNpgsql(Configuration.GetConnectionString("RRM_DB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
