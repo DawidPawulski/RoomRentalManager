2d09aad35ea64564d61a4a392756d2bf550ccb5c
JCKolodziej <49226039+JCKolodziej@users.noreply.github.com>
2020-02-26 22:01:43 +0100

    Add form with add new apartment, bug coming up, ask someone for help

--- RoomRentalManager.FrontEnd/src/views/NewApartmentPage.vue
+++ RoomRentalManager.FrontEnd/src/views/NewApartmentPage.vue
@@ -2,9 +2,9 @@
     <div>
         <Navbar :UserRole="UserRole" :UserID="UserID" />
         <div>
-            newapartment
+
             <div>
-                <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+                <b-form @submit="onSubmit" id="apartment-form">
                     <b-card bg-variant="light">
                         <b-form-group label="Apartment Adress Details"></b-form-group>
 
@@ -106,17 +106,19 @@
                         </b-form-group>
 
                         <b-form-group id="details-input-4">
-                            <b-form-checkbox-group switches
-                                                   stacked>
+                            <b-form-checkbox-group switches>
                                 <b-form-checkbox id="smoking"
                                                  v-model="form.smokingAllowed">Smoking Allowed</b-form-checkbox>
+                            </b-form-checkbox-group>
+                            <b-form-checkbox-group switches>
                                 <b-form-checkbox id="pets"
                                                  v-model="form.petsAllowed">Pets Allowed</b-form-checkbox>
                             </b-form-checkbox-group>
 
 
-                            <b-form-group v-if="form.rooms <= 1"
-                                          id="room-input">
+                            <b-form-group v-if="numberOfRooms <= 1"
+                                          id="room-input"
+                                          :key="numberOfRooms">
                                 <b-form-checkbox-group id="room-input-1"
                                                        switches
                                                        stacked>
@@ -151,18 +153,17 @@
                             </b-form-group>
 
                             <b-tabs v-else content-class="mt-3">
-                                <b-tab v-for="i in parseInt(this.form.rooms)" :key="i" title="Room {i+1}">
-                                    <b-form-group id="room{i + 1}-input">
-                                        <b-form-checkbox-group id="room{i + 1}-input-1"
+                                <b-tab v-for="i in parseInt(numberOfRooms)" :key="i" :title="i.toString()">
+                                    <b-form-group id="room-input">
+                                        <b-form-checkbox-group id="room-input-1"
                                                                switches
                                                                stacked>
                                             <b-form-checkbox id="furnished"
-                                                             v-model="room.furnished"
-                                                             value="furnished">Furnished</b-form-checkbox>
+                                                             v-model="room.furnished">Furnished</b-form-checkbox>
 
                                         </b-form-checkbox-group>
 
-                                        <b-form-group id="room{i + 1}-input-2"
+                                        <b-form-group id="room-input-2"
                                                       label="Monthly Amount:"
                                                       label-for="monthlyAmount">
                                             <b-form-input id="monthlyAmount"
@@ -172,7 +173,7 @@
                                                           placeholder="Monthly Amount"></b-form-input>
                                         </b-form-group>
 
-                                        <b-form-group id="room{i + 1}-input-3"
+                                        <b-form-group id="room-input-3"
                                                       label="Bank Account Number:"
                                                       label-for="bankAccount">
                                             <b-form-input id="bankAccount"
@@ -182,7 +183,18 @@
                                                           required
                                                           placeholder="Bank Account Number"></b-form-input>
                                         </b-form-group>
+                                        <b-form-group id="room-input-4"
+                                                      label="Room area:"
+                                                      label-for="roomArea">
+                                            <b-form-input id="roomArea"
+                                                          v-model="room.area"
+                                                          type="number"
+                                                          required
+                                                          placeholder="Bank Account Number"></b-form-input>
+                                        </b-form-group>
                                     </b-form-group>
+
+                                    <b-button typeof="button" :key="i" id="'save' + i.toString()" v-on:click="saveRoom()">Save</b-button>
                                 </b-tab>
 
                             </b-tabs>
@@ -229,25 +241,108 @@
                     year: '',
                     rooms: '',
                     smokingAllowed: '',
-                    petsAllowed: ''
+                    petsAllowed: '',
+                    roomsList: []
                 },
-                // figure out how to do number of rooms programatically
                 room: {
                     furnished: '',
                     monthlyAmount: '',
                     bankAccount: '',
                     area: ''
-                },
-                show: true
+                }
             }
         },
         methods: {
             onSubmit(evt) {
                 evt.preventDefault()
+                if (this.form.roomsList.length == 0) {
+                    this.room.area = this.form.area
+                    this.saveRoom()
+                }
+                // error coming up Error in v-on handler: "TypeError: Cannot read property 'then' of undefined"
+                // ask Piotr or Maciek for help with that
+                this.sendApartment().then(response => {
+                    this.form.roomsList.foreach((room) => {
+                        this.sendRoom(room, response.data.ApartmentId)
+                    })
+                })
+
                 alert(JSON.stringify(this.form))
+            },
+            // change it to recognize rooms later and update after another save or add edit button
+            saveRoom() {
+                let room = {
+                    furnished: this.room.furnished,
+                    monthlyAmount: this.room.monthlyAmount,
+                    bankAccount: this.room.bankAccount,
+                    area: this.room.area
+                }
+
+                this.form.roomsList.push(room)
+
+                if (this.form.roomsList.length > this.form.rooms) {
+                    this.form.roomsList.pop();
+                }
+            },
+            /* eslint-disable */
+            sendApartment() {
+                let date = new Date(this.form.year, 1, 1);
+                let smoking = false;
+                let pets = false;
+                if (this.form.smokingAllowed.length >0 && this.form.smokingAllowed[0] == true) {
+                    smoking = true;
+                }
+                if (this.form.petsAllowed.length > 0 && this.form.petsAllowed[0] == true) {
+                    pets = true;
+                }
+                const axios = require('axios');
+                let url = "https://localhost:44311/api/Apartments"
+                axios.post(url, {
+                    "UserID": parseInt(this.UserID),
+                    "NumberOfRooms": parseInt(this.form.rooms),
+                    "SmokingAllowed": smoking,
+                    "PetsAllowed": pets,
+                    "YearOfRenovation": date,
+                    "Floor": parseInt(this.form.floor),
+                    "City": this.form.city,
+                    "Street": this.form.street,
+                    "BuildingNumber": parseInt(this.form.buildingNumber),
+                    "ApartmentNumber": parseInt(this.form.apartmentNumber),
+                    "PostalCode": this.form.postalCode,
+                    "Area": parseInt(this.form.area)
+                }).then((response) => {
+                    console.log(response)
+                    return response
+                })
+            },
+            /* eslint-disable */
+            sendRoom(object, apartmentId) {
+                let furnished = false;
+                if (object.furnished.length > 0 && object.furnished[0] == true) {
+                    furnished = true;
+                }
+                const axios = require('axios');
+                let url = "https://localhost:44311/api/Rooms";
+                axios.post(url, {
+                    "ApartmentId": apartmentId,
+                    "Area": object.area,
+                    "Furnished": furnished,
+                    "MonthlyAmount": object.monthlyAmount,
+                    "BankAccountNumber": object.bankAccount
+                }).then((response) => {
+                    console.log(response)
+                    return response
+                })
             }
 
 
+
+        },
+        computed: {
+            numberOfRooms: function () {
+
+                return this.form.rooms;
+            }
         }
     }
 </script>
